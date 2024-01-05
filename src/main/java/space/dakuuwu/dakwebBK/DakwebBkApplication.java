943ffea9e90bd63d8f43ac2baf26a5eadf1d99fa
dakuuwu <dakuuwu@gmail.com>
2024-01-05 01:06:17 -0300

    Initial Spring Boot State

---
+++ src/main/java/space/dakuuwu/dakwebBK/DakwebBkApplication.java
@@ -0,0 +1,13 @@
+package space.dakuuwu.dakwebBK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakwebBkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakwebBkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/dakuuwu/dakwebBK/DakwebBkApplicationTests.java
@@ -0,0 +1,13 @@
+package space.dakuuwu.dakwebBK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DakwebBkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
