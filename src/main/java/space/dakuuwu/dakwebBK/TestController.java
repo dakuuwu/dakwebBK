5aaace02522114a77fbe1f21a88aefb81d778a53
dakuuwu <dakuuwu@gmail.com>
2024-01-06 17:06:18 -0300

    records??? what???

--- src/main/java/space/dakuuwu/dakwebBK/TestController.java
+++ src/main/java/space/dakuuwu/dakwebBK/TestController.java
@@ -20,58 +20,9 @@ public class TestController {
     }
 
 
-    public static class TestResponse {
-
-        private final String id;
-        private final TestResponseContent content;
-        private final String[] tags;
-
-        public TestResponse(String id, TestResponseContent content, String[] tags) {
-            this.id = id;
-            this.content = content;
-            this.tags = tags;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public TestResponseContent getContent() {
-            return content;
-        }
-
-        public String[] getTags() {
-            return tags;
-        }
+    public record TestResponse(String id, TestResponseContent content, String[] tags) {
     }
 
-    public static class TestResponseContent{
-        private final String title;
-        private final String imageurl;
-        private final String smalldesc;
-        private final String longdesc;
-
-        public TestResponseContent(String title, String imageurl, String smalldesc, String longdesc) {
-            this.title = title;
-            this.imageurl = imageurl;
-            this.smalldesc = smalldesc;
-            this.longdesc = longdesc;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getImageurl() {
-            return imageurl;
-        }
-
-        public String getSmalldesc() {
-            return smalldesc;
-        }
-
-        public String getLongdesc() {
-            return longdesc;
-        }
+    private record TestResponseContent(String title, String imageurl, String smalldesc, String longdesc) {
     }
 }
