8b6e4d96e314053fcbf3767fd7b8d28e62f09675
dakuuwu <dakuuwu@gmail.com>
2024-01-07 21:37:44 -0300

    Database/API/Schema working (POST, GET, PUT, DELETE) without CSRF and CORS setup (TODO)

--- src/main/java/space/dakuuwu/dakwebBK/PostController.java
+++ src/main/java/space/dakuuwu/dakwebBK/PostController.java
@@ -1,14 +1,60 @@
 package space.dakuuwu.dakwebBK;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import space.dakuuwu.dakwebBK.model.Post;
+import space.dakuuwu.dakwebBK.repos.PostsRepository;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
+@Validated
 public class PostController {
 
-    @GetMapping("/testpost")
-    public String test(){
-        return "post!";
+    @Autowired
+    private PostsRepository postsRepo;
+
+    //C
+    @PostMapping("/newpost")
+    public ResponseEntity<Post> createPost(@RequestBody Post p) {
+        Post savedPost = postsRepo.save(p);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPost);
     }
+
+    //R
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> postList = postsRepo.findAll();
+        return ResponseEntity.ok(postList);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> getPostbyID(@PathVariable String id) {
+        return postsRepo.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    //U
+    @PutMapping("/updatepost/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable String id, @RequestBody Post updatedPost) {
+        return postsRepo.findById(id)
+                .map(existingPost -> {
+                    existingPost.setContent(updatedPost.getContent());
+                    existingPost.setTags(updatedPost.getTags());
+                    Post savedPost = postsRepo.save(existingPost);
+                    return ResponseEntity.ok(savedPost);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //D
+    @DeleteMapping("/deletepost/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable String id) {
+        postsRepo.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/space/dakuuwu/dakwebBK/model/Post.java
+++ src/main/java/space/dakuuwu/dakwebBK/model/Post.java
@@ -1,32 +1,43 @@
 package space.dakuuwu.dakwebBK.model;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
 @Document(collection = "posts")
 public class Post {
-    private final ObjectId objectId;
-    private final PostContent content;
-    private final String[] tags;
+    private String id;
+    private PostContent content;
+    private String[] tags;
 
-    public Post(PostContent content, String[] tags) {
-        this.objectId = new ObjectId();
+    public Post(String id, PostContent content, String[] tags) {
+        this.id = id;
         this.content = content;
         this.tags = tags;
     }
 
-    public ObjectId getObjectId(){
-        return objectId;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public PostContent getContent() {
         return content;
     }
 
+    public void setContent(PostContent content) {
+        this.content = content;
+    }
+
     public String[] getTags() {
         return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
     }
 
     public record PostContent(String title, String imageurl, String smalldesc, String longdesc) {
