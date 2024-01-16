b55691434ed78ad299ad7c6091480817eb89d592
dakuuwu <dakuuwu@gmail.com>
2024-01-16 16:47:02 -0300

    Object restructuring, pending validation

--- src/main/java/space/dakuuwu/dakwebBK/services/DataValidationService.java
+++ src/main/java/space/dakuuwu/dakwebBK/services/DataValidationService.java
@@ -8,7 +8,7 @@ public class DataValidationService {
     public Boolean keyFieldValidator(Post p) {
         if (p.getContent().title().isBlank() || p.getContent().title() == null) {
             return false;
-        } else if (p.getContent().imageurl().isBlank() || p.getContent().imageurl() == null) {
+        } else if (p.getContent().imageUrl().isBlank() || p.getContent().imageUrl() == null) {
             return false;
         } else return p.getTags().length != 0 && p.getTags() != (null);
     }
